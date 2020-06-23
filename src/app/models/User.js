b4cc2f36d945cf81f1f2a6d96fd0ea3844c6d2cc
joseaugustodardengo <joseaugusto.dardengo@gmail.com>
2020-06-23 15:19:17 -0300

    Ajuste no User controller a partir da base model
    Criado o arquivo de base model; ajuste do User model e da user controller

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,73 +1,11 @@
 const db = require('../../config/db')
 const fs = require('fs')
+const Base = require('./Base')
+
+Base.init({table: 'users'})
 
 module.exports = {
-    async findAll(filters) {
-        try {
-            let query = `SELECT * FROM users`
-
-            const results = await db.query(query)
-
-            return results.rows
-        } catch (error) {
-            console.error(error)
-        }
-    },
-
-    async findOne(filters) {
-        try {
-            let query = `SELECT * FROM users`
-
-            Object.keys(filters).map(key => {
-                query = `${query} 
-                ${key}
-                `
-                Object.keys(filters[key]).map(field => {
-                    query = `${query} ${field} = '${filters[key][field]}' `
-                })
-            })
-
-            const results = await db.query(query)
-
-            return results.rows[0]
-        } catch (error) {
-            console.error(error)
-        }
-    },
-
-    async create(data) {
-        try {
-            const query = `INSERT INTO users (name, email, password, is_admin) VALUES ($1, $2, $3, $4)
-            RETURNING id`
-
-
-            const results = await db.query(query, data)
-            return results.rows[0].id
-
-        } catch (error) {
-            console.log(error)
-        }
-
-    },
-
-    async update(id, fields) {
-        let query = `UPDATE users SET`
-
-        Object.keys(fields).map((key, index, array) => {
-            if ((index + 1) < array.length) {
-                query = `${query} 
-                    ${key} = '${fields[key]}',
-                    `
-            } else {
-                query = `${query} 
-                    ${key} = '${fields[key]}'
-                    WHERE id = ${id}
-                    `
-            }
-        })
-
-        await db.query(query)
-    },
+    ...Base,    
 
     async delete(id) {
         try {
